5306eba331b5a33453bd8ead5091b778cc182342
Joel Leon Gfeller <joelleon.gfeller@icloud.com>
2024-06-18 17:53:10 +0100

    all conceivable board states

--- run.py
+++ run.py
@@ -33,3 +33,30 @@ def player_input():
 player1, player2 = player_input()
 
 print("Player1: "+player1+" Player2: "+player2)
+
+def win_check(board,mark):
+        return ((board[1] == mark and board[2] == mark and board[3] == mark) or
+            (board[4] == mark and board[5] == mark and board[6] == mark) or
+            (board[7] == mark and board[8] == mark and board[9] == mark) or
+            (board[1] == mark and board[4] == mark and board[7] == mark) or
+            (board[2] == mark and board[5] == mark and board[8] == mark) or
+            (board[3] == mark and board[6] == mark and board[9] == mark) or
+            (board[1] == mark and board[5] == mark and board[9] == mark) or
+            (board[3] == mark and board[5] == mark and board[7] == mark))
+
+
+def space_checker(board,position):
+     if board[position] == ' ':
+          return True
+     return False
+
+def marker_placer(board,position,mark):
+     board[position]=mark
+
+def player_choice(board):
+    position = 0
+
+    while position not in range(1,10) or not space_checker(board,position):
+        position = input('Place your marker enter 1-9: ')
+
+    return position 
